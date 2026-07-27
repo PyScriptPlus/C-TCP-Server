ec99eeb92bea4be4e00dcad1946f8deb04dc96fc
PyScriptPlus <mohamadexbier@gmail.com>
2026-07-28 01:11:37 +0330

    Add TCP server accept and message receiving

--- C_TCP_Server/main.c
+++ C_TCP_Server/main.c
@@ -6,17 +6,31 @@
 #include <netinet/in.h>
 
 #define PORT 2525
+#define MAX 1024
+
+void receiveCl(int *sockClient) {
+
+	char buff[1024];
+	int bytes = recv(*sockClient, buff, sizeof(buff) - 1, 0);
+	if(bytes > 0)
+	{
+		buff[bytes] = '\0';
+	}
+	printf("Client message: %s", buff);
+	return;
+}
+
 
 int main() {
 
 	int sockfd;
-	struct sockaddr_in servaddr;
+	struct sockaddr_in servaddr, client;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0)
 	{
 		// printf("Creation failed!\n");
-		perror("Creation failed!\n");
+		perror("Creation failed!");
 		return -1;
 	}
 
@@ -27,11 +41,31 @@ int main() {
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
-	if(bind(sockfd, (const struct sockaddr*)&servaddr, 
+	if(bind(sockfd, (struct sockaddr*)&servaddr, 
 		sizeof(servaddr)) < 0)
 	{
-		perror("bind failed\n!");
+		perror("bind failed!");
+		return -1;
 	}
+
+	int backlog = 10;
+	if(listen(sockfd, backlog) < 0)
+	{
+		perror("Listen failed!");
+		return -1;
+	}
+
+	memset(&client, 0, sizeof(client));
+	
+	socklen_t lenClient = sizeof(client);
+	int sockClient = accept(sockfd, (struct sockaddr*)&client, &lenClient);
+	if(sockClient < 0)
+	{
+		perror("Accept client failed!");
+		return -1;
+	}
+
+	receiveCl(&sockClient);
 
 	return 0;
 }
